432bcd7f7c84744b60d5bc75791681468eefc101
phattharaphorn <64010659@kmitl.ac.th>
2024-01-09 01:07:47 +0700

    Update item3

--- item-3/item3.py
+++ item-3/item3.py
@@ -1,11 +1,13 @@
 def water(wall:list):
     a = 0
     b = 1
-
     result = []
     while b < len(wall):
         if wall[a] > wall[b]:
-            b = max(range(b, len(wall)), key=wall.__getitem__)
+            for i in range(b, len(wall)):
+                if wall[i] > wall[b]:
+                    b = i
+            #b = max(range(b, len(wall)), key=wall.__getitem__)
             result.extend([min(wall[a], wall[b])] * (b - a))
         # elif wall[a] < wall[b]:
         #    a = max(range(a+1), key=wall.__getitem__)
@@ -13,7 +15,7 @@ def water(wall:list):
         else:
             result.append(wall[a])
         a = b 
-        b = a+1
-    return result
+        b = a+1 
+    return result 
 
 print(water([10,4,1,10,3,4,5, 9]))
