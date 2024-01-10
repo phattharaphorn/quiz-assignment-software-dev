a350396beae5b1d3e289aa350b217e4b831e8e69
phattharaphorn <64010659@kmitl.ac.th>
2024-01-10 21:32:48 +0700

    Update item1

--- item-1/item1.py
+++ item-1/item1.py
@@ -1,11 +1,14 @@
 def count_ways(n):
-    if n <= 1:
-        return n
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        return count_ways(n-1) + count_ways(n-2)
 
-    a, b = 0, 1
-    for _ in range(n):
-        a, b = b, a + b
+    #a, b = 0, 1
+    #for _ in range(n):
+    #   a, b = b, a + b
+    #return b
 
-    return b
-
-print(count_ways(8)) 
+print(count_ways(20)) 
